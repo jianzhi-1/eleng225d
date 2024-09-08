3e28ee4067b140dc7544d0fadbad6439e324b6e2
Jianzhi Wang <jianzhiw00@gmail.com>
2024-09-08 13:18:50 -0700

    Update startup.py

--- startup.py
+++ startup.py
@@ -2,8 +2,17 @@ import librosa # audio analysis
 import IPython.display as ipd
 
 wav, sr = librosa.load("file.mp3")
-# sr = sampling rate
+# sr = sampling rate (22050 for lower-quality audio files, the dimension of the array needs to be divided by sr to be in dimension of time)
 # wav = np.array (audio file is converted to time series), supports multichannel
+
+# Plotting an audio wave
+def plot(wav, sr):
+    plt.figure(figsize=(12, 8))
+    plt.plot(np.arange(len(wav))/sr, wav)
+    plt.xlabel("Time / s")
+    plt.ylabel("Amplitude / unit")
+    plt.title("Plot of Amplitude against Time")
+    plt.show()
 
 # Mel Spectrogram
 n_mels = 80
