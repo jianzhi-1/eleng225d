171f24a6cbe4dfa866b92aa8268ce7f16ea25f40
Jianzhi Wang <jianzhiw00@gmail.com>
2024-09-09 19:40:42 -0700

    Create transformers.py

---
+++ transformers.py
@@ -0,0 +1,11 @@
+import torch
+from transformers import pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model="openai/whisper-tiny",
+    chunk_length_s=30,
+    device=device,
+)
